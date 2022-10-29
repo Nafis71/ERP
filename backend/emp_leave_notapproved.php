827ed8a20bb26bccd44be12c294eda3a437085db
Nafis71 <nhtonmoy2@gmail.com>
2022-10-29 19:51:01 +0600

    added some code here

--- backend/emp_leave_notapproved.php
+++ backend/emp_leave_notapproved.php
@@ -3,7 +3,16 @@ session_start();
 include 'connect.php';
 mysqli_select_db($connect,'erp');
 $id = $_GET['id'];
+if($page!=2)
+{
 $query = "UPDATE emp_leave set approve_status = '0' where emp_id ='$id'";
 mysqli_query($connect,$query);
 header("location:../hrm/emp_leave.php");
+}
+else
+{
+    $query = "UPDATE emp_leave set approve_status = '0' where emp_id ='$id'";
+mysqli_query($connect,$query);
+header("location:../hrm/emp_leave_search.php?search=$id");
+}
 ?>
